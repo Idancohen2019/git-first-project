9c54a31e82668d79f71c5e2f99b56e9c0b1b20fa
Idancohen2019 <41032041+Idancohen2019@users.noreply.github.com>
2020-08-24 16:42:19 +0300

    please upload

---
+++ 1.py
@@ -0,0 +1,29 @@
+
+###EXRESIZE(lesson number 1)###
+#A
+first = 7
+secound = 44.3
+print(first + secound)
+print(first * secound)
+print(secound / first)
+
+#B
+a = 9
+c = 15
+b = 8
+
+###c there is no differnts it's the same, just a diffrent way to do string#
+
+my_number = 5+5
+print("result is :" + str(my_number))
+
+#D
+#7 because the int is not float
+x = 5
+y = 2.36
+print(x+int(y))
+
+#Chalenge
+a = 8
+b ="123"
+print(a + int(b))
